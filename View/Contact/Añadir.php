5b132c113049cc29e7d5352829c0ae5243521c45
asier-aldeekoa <a_aldekoa@fpzornotza.com>
2024-03-15 16:43:56 +0100

    MAs cambios y vamos avanzando

--- View/Contact/Añadir.php
+++ View/Contact/Añadir.php
@@ -1,33 +1,3 @@
-<!-- Formulario De Registro -->
-
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Formulario de Registro</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-        <link rel="icon" type="image/x-icon" href="../Public/Images/Logo.jpg">
-    </head>
-    <body>
-        <div class="container">
-            <h1>Formulario de Registro</h1>
-            <form>
-                <div class="mb-3">
-                    <label for="usuario" class="form-label">Usuario</label>
-                    <input type="text" class="form-control" id="usuario" name="usuario">
-                </div>
-                <div class="mb-3">
-                    <label for="contrasena" class="form-label">Contraseña</label>
-                    <input type="password" class="form-control" id="contrasena" name="contrasena">
-                </div>
-                <button type="submit" class="btn btn-primary">Registrarse</button>
-            </form>
-        </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    </body>
-</html>
-
 <!-- Formulario De Añadir Contacto -->
 
 <!DOCTYPE html>
