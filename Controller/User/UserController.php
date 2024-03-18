95afa57ef2eff0606fd0a7f50cdbd8d79202b8da
asier-aldeekoa <a_aldekoa@fpzornotza.com>
2024-03-18 11:40:05 +0100

    Eliminado archivo workspace.xml y agregado redireccionamiento en UserController.php

--- Controller/User/UserController.php
+++ Controller/User/UserController.php
@@ -1,4 +1,10 @@
 <?php
+if (isset($_POST['Volver'])) {
+    header("Location: http://$_SERVER[HTTP_HOST]/View/User/Login.php");
+    exit();
+}
+
+// Luego, el resto de tu código continua...
 include_once '../Model/Database/Database.php';
 include_once '../Model/contactModel.php';
 include_once "../View/User/Registro.php";
@@ -11,3 +17,4 @@ if (isset($_POST['Registro'])) {
     header("Location: http://$_SERVER[HTTP_HOST]/View/User/Registro.php");
     exit();
 }
+?>
