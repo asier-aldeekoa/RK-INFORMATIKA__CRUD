e18370536fd767755ba70631dea44fa727167889
asier-aldeekoa <a_aldekoa@fpzornotza.com>
2024-03-19 18:28:22 +0100

    Añadido favicon.ico a las páginas de Añadir Contacto y Registro

--- View/Contact/Añadir.php
+++ View/Contact/Añadir.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario de Añadir Contacto</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="icon" type="image/x-icon" href="../../Public/Images/favicon.ico">
 </head>
 <body>
     <div class="container">
