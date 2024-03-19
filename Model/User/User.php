0112f5e77256337622f2bab242c7a82eab9cdf9d
asier-aldeekoa <a_aldekoa@fpzornotza.com>
2024-03-19 10:13:36 +0100

    Eliminar clase UsuarioModelo y archivo Select.php de Contactos

--- Model/User/User.php
+++ Model/User/User.php
@@ -12,44 +12,4 @@ class UsuarioModelo{
             echo $e->getMessage();
         }
     }
-
-    public function Alta($Erabiltzailea, $Pasahitza) {
-        try {
-            $this->konektatu();
-            $hashedPassword = password_hash($Pasahitza, PASSWORD_DEFAULT);
-            $sql = "INSERT INTO users(username, password) VALUES (?, ?);";
-            $stmt = $this->mysqli->prepare($sql);
-            $stmt->bind_param("ss", $Erabiltzailea, $hashedPassword);
-            $stmt->execute();
-            
-            if ($stmt->affected_rows == 1) {
-                return true;
-            } else {
-                return false;
-            }
-        } catch (Exception $ex) {
-            throw $ex;
-        }
-    }
-
-
-    public function usuarioExiste($username) {
-        $stmt = $this->mysqli->prepare("SELECT idUser FROM users WHERE username = ?");
-        // Cambié "usuario" por "username" en la consulta preparada
-        $stmt->bind_param("s", $username);
-        $stmt->execute();
-        $stmt->store_result();
-        $exists = $stmt->num_rows > 0;
-        $stmt->close();
-        return $exists;
-    }
-
-    public function getMysqli() {
-        return $this->mysqli;
-    }
-    
-    public function getLastInsertedUserId() {
-        // Asumiendo que $mysqli es tu conexión a la base de datos
-        return mysqli_insert_id($this->getMysqli());
-    }
 }
